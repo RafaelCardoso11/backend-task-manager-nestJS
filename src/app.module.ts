296b9f7819d7069243a47ae261b3fcc027892614
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-09-29 20:27:29 -0300

    feat: config db postgres from the environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [TaskModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TaskModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      database: process.env.DB_DATABASE,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      entities: [],
+      synchronize: process.env.ENV === 'development',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
