cf16d102a14170054d14671db991049c8d9bfde8
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-09-29 21:30:43 -0300

    feat: task dto and entity

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,1 +1,17 @@
-export class CreateTaskDto {}
+import { IsDateString, IsEnum, IsOptional, IsString } from 'class-validator';
+import { PriorityEnum } from 'src/enums/priority.enum';
+
+export class CreateTaskDto {
+  @IsString()
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  description: string;
+
+  @IsEnum(PriorityEnum)
+  priority: string;
+
+  @IsDateString()
+  dueDate: string;
+}
--- src/task/entities/task.entity.ts
+++ src/task/entities/task.entity.ts
@@ -1,1 +1,46 @@
-export class Task {}
+import { PriorityEnum } from 'src/enums/priority.enum';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({
+  name: 'task',
+})
+export class TaskEntity {
+  @PrimaryGeneratedColumn({
+    unsigned: true,
+  })
+  id: number;
+
+  @Column({
+    length: 127,
+  })
+  title: string;
+
+  @Column({
+    length: 300,
+    nullable: true,
+  })
+  description: string;
+
+  @Column({
+    enum: PriorityEnum,
+  })
+  priority: string;
+
+  @Column({
+    type: 'date',
+    nullable: false,
+  })
+  dueDate: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
