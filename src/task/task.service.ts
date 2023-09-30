a195f642527c9d95919a1d7a082d6d888a421bd9
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-09-29 21:30:25 -0300

    feat: task service and module

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,26 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { Repository } from 'typeorm';
+import { TaskEntity } from './entities/task.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class TaskService {
-  create(createTaskDto: CreateTaskDto) {
-    return 'This action adds a new task';
+  constructor(
+    @InjectRepository(TaskEntity)
+    private readonly taskRepository: Repository<TaskEntity>,
+  ) {}
+  async create(createTaskDto: CreateTaskDto) {
+    const task = this.taskRepository.create(createTaskDto);
+
+    await this.taskRepository.save(task);
+
+    return task;
   }
 
-  findAll() {
-    return `This action returns all task`;
+  async findAll() {
+    const tasks = await this.taskRepository.find();
+    return {
+      data: tasks,
+      totalCounts: tasks.length,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} task`;
+  async findOne(id: number) {
+    const task = await this.taskRepository.findOne({ where: { id } });
+
+    if (!task) {
+      throw new BadRequestException('Task not found');
+    }
+    return {
+      data: task,
+    };
   }
 
-  update(id: number, updateTaskDto: UpdateTaskDto) {
-    return `This action updates a #${id} task`;
+  async update(id: number, updateTaskDto: UpdateTaskDto) {
+    const { affected } = await this.taskRepository.update(id, updateTaskDto);
+
+    if (!!!affected) {
+      throw new NotFoundException(`Registration for ${id} not found`);
+    }
+
+    return {
+      data: updateTaskDto,
+      message: 'Task update successfully',
+    };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} task`;
+  async remove(id: number) {
+    const { affected } = await this.taskRepository.delete(id);
+
+    if (!!!affected) {
+      throw new NotFoundException(`Registration for ${id} not found`);
+    }
+
+    return {
+      message: 'Task deleted successfully',
+    };
   }
 }
