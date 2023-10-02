54869608d1cfaac45a7e475da3f0c8e6bd1ed690
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-10-02 02:08:02 -0300

    feat: MaxLength title and description

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,12 +1,20 @@
-import { IsDateString, IsEnum, IsOptional, IsString } from 'class-validator';
+import {
+  IsDateString,
+  IsEnum,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
 import { minDateISO } from 'src/decorators/minDateISO.decorator';
 import { PriorityEnum } from 'src/enums/priority.enum';
 
 export class CreateTaskDto {
+  @MaxLength(127)
   @IsString()
   title: string;
 
   @IsOptional()
+  @MaxLength(300)
   @IsString()
   description: string;
 
